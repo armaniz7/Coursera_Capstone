90fffeb4983259ca5cb4bcc0ac759115bb6e3a4d
armaniz7 <74456131+armaniz7@users.noreply.github.com>
2020-12-19 12:09:31 +0300

    Add files via upload

---
+++ Capstone Project Course.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the capstone project.
+
+# In[6]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[7]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
